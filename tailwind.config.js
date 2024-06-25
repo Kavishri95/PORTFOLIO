0ef29c551b9ef38dedf25d20eb170346690bd69b
KAVISHRI S <162982700+Kavishri95@users.noreply.github.com>
2024-06-25 10:53:02 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const { RiGitBranchFill } = require('react-icons/ri');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+        },
+    fontFamily:{
+      'heroFont' :'Sriracha' 
+    }
+  },
+  plugins: [],
+}
+
